a9fddafd7383926b52c044d299241d6204c702dd
Wellington Albino <well.renato@hotmail.com>
2024-06-12 15:58:59 -0300

    Criando a classe conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,11 +2,19 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		Scanner scanner = new Scanner(System.in);
 		
 		int opcao;
@@ -14,25 +22,23 @@ public class Menu {
 		while(true) {
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
-			
+			System.out.println("                                                     ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
-			System.out.println("");
-			System.out.println("\t\tBANCO DO BAZIL COM Z\t\t");
-			System.out.println("");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
-			System.out.println("");
-			System.out.println("\t\t1 - Criar conta\t\t");
-			System.out.println("\t\t2 - Listar todas as contas\t\t");
-			System.out.println("\t\t3 - Buscar conta por número\t\t");
-			System.out.println("\t\t4 - Atualizar dados da conta\t\t");
-			System.out.println("\t\t5 - Apagar conta\t\t");
-			System.out.println("\t\t6 - Sacar\t\t");
-			System.out.println("\t\t7 - Depositar\t\t");
-			System.out.println("\t\t8 - Transferir valores entre contas\t\t");
-			System.out.println("\t\t9 - Sair\t\t");
-			System.out.println("");
-			System.out.println("*****************************************************");
-			System.out.println("\t\tEntre com a opção desejada: \t\t");
+			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
 			opcao = scanner.nextInt();
 			
